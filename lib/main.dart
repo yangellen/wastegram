49884838108bee3f9543451e7dd254fdf4ef4dda
Ellen Yang <yange@oregonstate.edu>
2021-03-04 23:41:01 -0800

    able to get data from firestore database

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ListScreen extends StatefulWidget {
   @override
@@ -8,14 +9,38 @@ class ListScreen extends StatefulWidget {
 class _ListScreenState extends State<ListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Wastegram"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+      builder: (context, snapshots) {
+        if (snapshots.hasData &&
+            snapshots.data.documents != 0 &&
+            snapshots.data.documents.length > 0) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Wastegram'),
+              centerTitle: true,
+            ),
+            body: ListView.builder(
+                itemCount: snapshots.data.documents.length,
+                itemBuilder: (context, index) {
+                  var post = snapshots.data.documents[index];
+                  return ListTile(
+                    title: Text(post['quantity'].toString()),
+                  );
+                }),
+          );
+        } else {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Wastegram'),
+              centerTitle: true,
+            ),
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
     );
   }
 }
