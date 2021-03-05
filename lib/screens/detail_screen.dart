28d6e23dbce75fc634f5a4f9944ebcaf8750d419
Ellen Yang <yange@oregonstate.edu>
2021-03-05 10:37:49 -0800

    add detail screen

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class DetialScreen extends StatelessWidget {
+  final date;
+  final imageUrl;
+  final quantity;
+  final latitude;
+  final longtitude;
+
+  DetialScreen(
+      {this.date,
+      this.imageUrl,
+      this.quantity,
+      this.latitude,
+      this.longtitude});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Wastegram"),
+        centerTitle: true,
+      ),
+      body: displayDetail(),
+    );
+  }
+
+  //display journal detail
+  Widget displayDetail() {
+    return ListView(
+      children: [
+        Container(
+          child: Center(
+            child: Text(date),
+          ),
+        ),
+        Container(
+          child: Center(
+            child: Text(imageUrl),
+          ),
+        ),
+        Container(
+          child: Center(
+            child: Text('$quantity items'),
+          ),
+        ),
+        Container(
+          child: Center(
+            child: Text('Location: ($latitude, $longtitude'),
+          ),
+        ),
+      ],
+    );
+  }
+}
